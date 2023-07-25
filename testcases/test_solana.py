f10c58f8556a59cb85c91b7bc1ef119451f84eba
xueyanli <xueyanli19950914@gmail.com>
2023-07-25 14:42:12 +0800

    add --fee-payer

--- testcases/test_solana.py
+++ testcases/test_solana.py
@@ -24,15 +24,16 @@ class TestSolana:
 
     def test_signMessage(self):
         self.message_value, self.public_keys, self.signatures = solana_signMessage(
-            "/tmp/keypair.json", self.msgToSign, self.pubkey)
+            "/tmp/keypair.json", self.msgToSign, self.pubkey, "/tmp/keypair.json")
 
         assert self.message_value is not None
-        assert self.public_keys[1] == self.pubkey
+        for i in range(len(self.public_keys)):
+            assert self.public_keys[i] == self.pubkey
         assert self.signatures is not None
 
     def test_verifyMessage(self):
         self.message_value, self.public_keys, self.signatures = solana_signMessage(
-            "/tmp/keypair.json", self.msgToSign, self.pubkey)
+            "/tmp/keypair.json", self.msgToSign, self.pubkey, "/tmp/keypair.json")
         for i in range(len(self.public_keys)):
             result = verifyMsgForSolana(self.public_keys[i], self.signatures[i], self.message_value)
             assert "Signature verification succeeded!" in result
