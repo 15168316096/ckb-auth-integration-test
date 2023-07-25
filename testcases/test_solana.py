5693c72c69e81aca62329d261a954b1edace7b93
xueyanli <xueyanli19950914@gmail.com>
2023-07-25 15:02:57 +0800

    fix ci

--- testcases/test_solana.py
+++ testcases/test_solana.py
@@ -4,7 +4,6 @@ from framework.helper.solana_cli import *
 
 
 class TestSolana:
-
     pubkey = solana_keygen()
 
     @classmethod
@@ -17,15 +16,13 @@ class TestSolana:
         cls.msgToSign = get_messageForSolana(cls.pubKeyHash)
         cls.message_value = cls.public_keys = cls.signatures = None
 
-
     @classmethod
     def teardown_class(cls):
         clean("solana")
 
     def test_signMessage(self):
         self.message_value, self.public_keys, self.signatures = solana_signMessage(
-            "/tmp/keypair.json", self.msgToSign, self.pubkey, "/tmp/keypair.json")
-
+            "/tmp/keypair.json", self.msgToSign, self.pubkey)
         assert self.message_value is not None
         for i in range(len(self.public_keys)):
             assert self.public_keys[i] == self.pubkey
@@ -33,7 +30,7 @@ class TestSolana:
 
     def test_verifyMessage(self):
         self.message_value, self.public_keys, self.signatures = solana_signMessage(
-            "/tmp/keypair.json", self.msgToSign, self.pubkey, "/tmp/keypair.json")
+            "/tmp/keypair.json", self.msgToSign, self.pubkey)
         for i in range(len(self.public_keys)):
             result = verifyMsgForSolana(self.public_keys[i], self.signatures[i], self.message_value)
             assert "Signature verification succeeded!" in result
