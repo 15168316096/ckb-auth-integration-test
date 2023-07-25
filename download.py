5992e9bb875a5b6c8fc3a557715fb881ea5c299a
xueyanli <xueyanli19950914@gmail.com>
2023-07-25 10:43:01 +0800

    framework commit solana

--- download.py
+++ download.py
@@ -64,7 +64,7 @@ class Solana(Blockchain):
                "-apple-darwin.tar.bz2"
 
     def print_help(self):
-        subprocess.run(["solana", "--help"])
+        subprocess.run(["./solana", "--help"])
 
 
 class Monero(Blockchain):
@@ -75,7 +75,7 @@ class Monero(Blockchain):
         return "https://downloads.getmonero.org/cli/monero-linux-x64-v0.18.2.2.tar.bz2"
 
     def print_help(self):
-        subprocess.run(["monero-wallet-cli", "--help"])
+        subprocess.run(["./monero-wallet-cli", "--help"])
 
 
 class Litecoin(Blockchain):
@@ -86,4 +86,4 @@ class Litecoin(Blockchain):
         return "https://download.litecoin.org/litecoin-0.21.2.2/linux/litecoin-0.21.2.2-x86_64-linux-gnu.tar.gz"
 
     def print_help(self):
-        subprocess.run(["litecoin-cli", "--help"])
+        subprocess.run(["./itecoin-cli", "--help"])
