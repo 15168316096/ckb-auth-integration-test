b9afaa56533ca1b74092b29acdc34c0429bf70d0
xueyanli <xueyanli19950914@gmail.com>
2023-08-01 17:42:27 +0800

    supoort ripple and ci test

--- testcases/test_ripple.py
+++ testcases/test_ripple.py
@@ -6,21 +6,24 @@ class TestRipple:
 
     @classmethod
     def setup_class(cls):
-        start_service()
-        cls.account_id, cls.master_seed = get_account_id_and_master_seed()
-        cls.ckb_message_hash = get_message_hash_ripple()
+        cls.DOCKER_RIPPLED_NAME = start_service()
 
     @classmethod
     def teardown_class(cls):
         stop_service()
 
     def test_signMessage(self):
-        tx_blob = get_tx_blob_from_rippled_sign(self.master_seed, self.ckb_message_hash)
+        print(f"debug:{self.DOCKER_RIPPLED_NAME}")
+        self.account_id, self.master_seed = get_account_id_and_master_seed(self.DOCKER_RIPPLED_NAME)
+        self.ckb_message_hash = get_message_hash_ripple()
+        tx_blob = get_tx_blob_from_rippled_sign(self.master_seed, self.ckb_message_hash, self.DOCKER_RIPPLED_NAME)
         assert tx_blob is not None
 
     def test_verifyMessage(self):
+        self.account_id, self.master_seed = get_account_id_and_master_seed(self.DOCKER_RIPPLED_NAME)
+        self.ckb_message_hash = get_message_hash_ripple()
         result = verify_ripple_signature(self.account_id,
-                                get_tx_blob_from_rippled_sign(self.master_seed, self.ckb_message_hash),
-                                )
+                                         get_tx_blob_from_rippled_sign(self.master_seed, self.ckb_message_hash,
+                                                                       self.DOCKER_RIPPLED_NAME),
+                                         )
         assert "Signature verification succeeded" in result
-
