23ea32ef96c2892e7db98e408c8b26bb6c08e668
iu小迷弟 <15168316096@163.com>
2023-11-10 09:59:52 +0000

    refactor ckb-auth

--- prepare.sh
+++ prepare.sh
@@ -1,21 +1,17 @@
 set -e
 echo "get capsule "
 rustup update
-cargo install cross --git https://github.com/cross-rs/cross --force
-cargo install ckb-capsule --git https://github.com/nervosnetwork/capsule.git --tag v0.9.2 --force
+cargo install cross --git https://github.com/cross-rs/cross --force --branch main
+cargo install ckb-capsule --git https://github.com/nervosnetwork/capsule.git --tag v0.10.2 --force
 
 echo "download ckb-auth"
 git clone https://github.com/joii2020/ckb-auth.git
 cd ckb-auth
-git checkout tron
+git checkout refactoring
 
 echo "build contract"
 git submodule update --init
 make all-via-docker
-
-echo "compile auth-demo"
-capsule build
-make -f examples/auth-demo/Makefile all-via-docker
 
 echo "build ckb-auth-cli"
 cd tools/ckb-auth-cli && cargo build
