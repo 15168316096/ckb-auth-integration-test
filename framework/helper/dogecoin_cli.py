61f1c85b152dbfb427732df44a214df9385db6cd
xueyanli <xueyanli19950914@gmail.com>
2023-11-21 22:38:31 +0800

    init

--- framework/helper/dogecoin_cli.py
+++ framework/helper/dogecoin_cli.py
@@ -26,10 +26,10 @@ def installDogecoinCore():
 
 def get_dogecoin_address(dogecoin_cli):
     try:
-        cmd = f'cd {dogecoin_cli} && ./dogecoin-cli getaddressesbyaccount ""'
-        print(f"cmd:{cmd}")
-        result = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
-        addresses = result.stdout.strip().replace('[', '').replace(']', '').replace('"', '').split(', ')
+        # cmd = f'cd {dogecoin_cli} && ./dogecoin-cli getaddressesbyaccount ""'
+        # print(f"cmd:{cmd}")
+        # result = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
+        # addresses = result.stdout.strip().replace('[', '').replace(']', '').replace('"', '').split(', ')
         # return addresses[0]
         return "DDjoXsNFMo8iu59mkKF45ddcFoECUehovB"
     except subprocess.CalledProcessError as e:
@@ -37,12 +37,12 @@ def get_dogecoin_address(dogecoin_cli):
 
 
 def generateSignature(dogecoin_cli, address):
-    message = generateBytes()
-    print(f"message:{message}")
-    signMessage = f"cd {dogecoin_cli} && ./dogecoin-cli signmessage {address} {message}"
-    result = subprocess.run(f"{signMessage}", shell=True, stdout=subprocess.PIPE, text=True)
-    output = result.stdout.strip()
-    print(f"sign message: {output}")
+    # message = generateBytes()
+    # print(f"message:{message}")
+    # signMessage = f"cd {dogecoin_cli} && ./dogecoin-cli signmessage {address} {message}"
+    # result = subprocess.run(f"{signMessage}", shell=True, stdout=subprocess.PIPE, text=True)
+    # output = result.stdout.strip()
+    # print(f"sign message: {output}")
     # return output, message
     return "IMdNZFx8IJpU0Omshlh//2HmqjbN89I29NK7zg6CvDByLR2Mq2zBoL864EY6XBdf4e2fmBobwtRghEeSq/fVWnA=", \
     "3d3f948b1f62a577673d489adbd1ab7a0da9d39e58938d1a20e598d041da1b5c"
