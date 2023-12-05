67f1181f1549cc377c791fa2edd88996d24ca8eb
iu小迷弟 <15168316096@163.com>
2023-12-05 10:38:27 +0000

    add prepare requirements

--- prepare.sh
+++ prepare.sh
@@ -1,17 +1,22 @@
 set -e
 echo "get capsule "
-rustup update
-cargo install cross --git https://github.com/cross-rs/cross --force --branch main
-cargo install ckb-capsule --git https://github.com/nervosnetwork/capsule.git --tag v0.10.2 --force
+# rustup update
+# cargo install cross --git https://github.com/cross-rs/cross --force --branch main
+# cargo install ckb-capsule --git https://github.com/nervosnetwork/capsule.git --tag v0.10.2 --force
 
 echo "download ckb-auth"
-git clone https://github.com/joii2020/ckb-auth.git
+git clone https://github.com/nervosnetwork/ckb-auth.git
 cd ckb-auth
-git checkout dogecoin
+git checkout main
 
 echo "build contract"
 git submodule update --init
 make all-via-docker
 
-echo "build ckb-auth-cli"
-cd tools/ckb-auth-cli && cargo build
+echo "build auth-rust-demo"
+cd examples/auth-rust-demo
+capsule build
+
+# echo "build ckb-auth-cli"
+# cd tools/ckb-auth-cli && cargo build
+
